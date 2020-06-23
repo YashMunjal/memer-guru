9b8494147601e6f8a69d1b363c148d28525a859b
yashmunjal <yashmu45@gmail.com>
2020-06-23 18:44:44 +0530

    started

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 module.exports = function () {
-  fetch("https://meme-api.herokuapp.com/gimme")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  console.log('First Package');
 };
