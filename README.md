6dc4b0a9f11f915f9a12413a45cfc470f578d91f
yashmunjal <yashmu45@gmail.com>
2020-06-23 23:17:45 +0530

    finished

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # memer-guru
-Trying to Publish an NPM Package that will provide with memes link on each callback
+Trying to Publish an NPM Package that will provide with random reddit memes link on each callback
+
+The package uses asyncronous callbacks from rapidAPi meme apis
+
+use it inside any image src to get the meme
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = function () {
-  console.log('First Package');
-};
+
+
+ async function meme() {
+  var url = "https://meme-api.herokuapp.com/gimme";
+  var srcData;
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      srcData = data.url;
+      
+    });
+    return `<img src=${JSON.stringify(srcData)} />` ;
+   
+}
+export default meme();
